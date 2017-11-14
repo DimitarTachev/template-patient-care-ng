ada5909be9297c8c9cb21992059e7b958a1d14af
Alexander Djenkov <djenkov@progress.com>
2017-11-14 14:00:43 +0200

    Add tab navigation.Move connect and care-card to tabs.

--- app/tabs/tabs-routing.module.ts
+++ app/tabs/tabs-routing.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
-import { ConnectComponent } from "./connect.component";
+import { TabsComponent } from "./tabs.component";
 
 const routes: Routes = [
-    { path: "", component: ConnectComponent },
-    { path: "connect", component: ConnectComponent }
+    { path: "", component: TabsComponent }
 ];
 
 @NgModule({
     imports: [NativeScriptRouterModule.forChild(routes)],
     exports: [NativeScriptRouterModule]
 })
-export class ConnectRoutingModule { }
+export class TabsRoutingModule { }
